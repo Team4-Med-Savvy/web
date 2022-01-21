d62a1a2acc84b167e30dd979c4338b6060ca3a47
Sourav Borah <souravborah@Souravs-MacBook-Pro.local>
2022-01-21 13:06:59 +0530

    about page edit and footer component

--- src/components/About.vue
+++ src/components/About.vue
@@ -6,6 +6,55 @@
     <div class="subnav-bar">
       <SubNavbar/>
     </div>
+    <div class="content-area">
+      <div class="content">
+        <h1>About Us</h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Enim incidunt minima, ratione iste veritatis voluptas, molestias facilis at sit quos dicta atque cupiditate architecto, magnam quaerat non accusamus modi adipisci?</p>
+      </div>
+      <div class="content-image">
+        <img src="@/assets/logo.jpeg" alt="">
+      </div>
+    </div>
+    <div class="namelist-2">
+      <div class="container">
+      <img src="@/assets/gandhi.jpeg" alt="Avatar" class="image" height="50px" width="50px">
+      <div class="middle">
+        <div class="text">Gandhi</div>
+      </div>
+    </div>
+    <div class="container">
+      <img src="@/assets/gandhi.jpeg" alt="Avatar" class="image" height="50px" width="50px">
+      <div class="middle">
+        <div class="text">Gandhi</div>
+      </div>
+    </div>
+    <div class="container">
+      <img src="@/assets/gandhi.jpeg" alt="Avatar" class="image" height="50px" width="50px">
+      <div class="middle">
+        <div class="text">Gandhi</div>
+      </div>
+    </div>
+    </div>
+    <div class="namelist-1">
+      <div class="container">
+      <img src="@/assets/gandhi.jpeg" alt="Avatar" class="image" height="50px" width="50px">
+      <div class="middle">
+        <div class="text">Gandhi</div>
+      </div>
+    </div>
+    <div class="container">
+      <img src="@/assets/gandhi.jpeg" alt="Avatar" class="image" height="50px" width="50px">
+      <div class="middle">
+        <div class="text">Gandhi</div>
+      </div>
+    </div>
+    <div class="container">
+      <img src="@/assets/gandhi.jpeg" alt="Avatar" class="image" height="50px" width="50px">
+      <div class="middle">
+        <div class="text">Gandhi</div>
+      </div>
+    </div>
+    </div>
   </div>
 </template>
 <script>
@@ -20,6 +69,78 @@ export default {
 }
 </script>
 <style scoped>
-.main-body{
+img{
+  border-radius: 50%;
+}
+.content-area{
+  margin:50px;
+  display: flex;
+  justify-content: space-between;
+  height: 150px;
+}
+.content{
+  margin-left: 150px;
+  width: 600px;
+  margin-bottom:150px;
+}
+.content-image{
+  margin-right: 150px;
+}
+.container {
+  position: relative;
+  width: 25%;
+}
+
+.image {
+  opacity: 1;
+  display: block;
+  width: 50%;
+  height: auto;
+  transition: .5s ease;
+  backface-visibility: hidden;
+}
+
+.middle {
+  transition: .5s ease;
+  opacity: 0;
+  position: absolute;
+  top: 35%;
+  left: 20%;
+  transform: translate(-35%, -20%);
+  -ms-transform: translate(-35%, -20%);
+  text-align: center;
+}
+
+.container:hover .image {
+  opacity: 0.3;
+}
+
+.container:hover .middle {
+  opacity: 1;
+}
+
+.text {
+  background-color: #000000;
+  color: white;
+  font-size: 12px;
+  padding: 16px 32px;
+}
+.namelist-1{
+  padding-left:100px;
+  display: flex;
+  justify-content: space-between;
+}
+.namelist-2{
+  padding-left:100px;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom:30px;
+  margin-top:150px;
+}
+
+.namelist-1{
+  padding-left:100px;
+  display: flex;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <footer>
+      <link rel="stylesheet"
+        href="https://use.fontawesome.com/releases/v5.2.0/css/all.css"
+        integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ"
+        crossorigin="anonymous">
+      <div class="container-fluid text-center" style="background-color: black">
+          <div class="row pt-5" style="background-color: black;">
+                <div class="col-12 col-md-4 pb-5">
+                    <p class="text-white font-weight-bold pb-3 h5 d-block">MEDSAVVY</p>
+                    <p class="text-white font-weight-bold pb-3 d-block">We are offering Helath Care Services of Five different Categories</p>
+                </div>
+                <div class="col-12 col-md-4 pb-5">
+                    <p class="text-white font-weight-bold pb-3 h5 d-block">USEFUL LINKS</p>
+                    <a href="https://www.w3schools.com/css/" class="text-white font-weight-bold pb-3 d-block">Refer CSS Here</a>
+                    <a href="https://getbootstrap.com/" class="text-white font-weight-bold pb-3 d-block"> Refer BootStrap Here</a>
+                    <a href="https://vuejs.org/" class="text-white font-weight-bold pb-3 d-block">Refer VueJs Here</a>
+                </div>
+                <div class="col-12 col-md-4 pb-5">
+                  <p class="text-white font-weight-bold pb-3 h5 d-block">GITHUB LINK</p>
+                  <a class="bg-white text-black fa-2x" href="https://github.com"><i class="fab fa-github"></i></a>
+                </div>
+            </div>
+        <div class="text-white font-weight-bold py-1">
+                <p>Copyright © Medsavvy 2022</p>
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
+<style scoped>
+p{
+  font-family:'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
