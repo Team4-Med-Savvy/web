d18b8e3538b7e83ce9480c8be32b66885131a532
Vijaya Shukla <vijayashukla@Vijayas-MacBook-Pro.local>
2022-01-21 18:00:51 +0530

    login1

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,54 +1,100 @@
 <template>
-  <div class="d-flex align-items-center light-blue-gradient" style="height: 700px;">
-<div class="container" >
-<div class="d-flex justify-content-center">
-<div class="col-md-5">
-<div class="card rounded-0 shadow">
-<div class="card-body">
-<h3 class="h3class">Log in</h3>
-<form>
-<div class="form-group">
-<label for="exampleInputEmail1">Email address:</label>
-<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-</div>
-<div class="form-group">
-<label for="exampleInputPassword1">Password: </label>
-<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-</div>
-<div class="form-group">
-<label for="exampleInputPassword1">Confirm Password:</label>
-<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-</div>
-<div class="submit-button">
-  <b-button type="submit" variant="outline-dark">Log in</b-button>
-</div>
-<RuleBox>
-<Hr /> <div style="text-align:center">or</div> <Hr />
-</RuleBox>
-<div class="submit-button">
-  <button variant="outline-dark">
-  <img src="@/assets/google.png" /> Login with Google
-</button>
-</div>
-
-</form>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
+  <div class="main-body">
+    <div>
+      <Navbar />
+    </div>
+    <div>
+      <SubNavBar />
+    </div>
+    <div
+      class="d-flex align-items-center light-blue-gradient"
+      style="height: 700px;"
+    >
+      <div class="container">
+        <div class="d-flex justify-content-center">
+          <div class="col-md-5">
+            <div class="card rounded-0 shadow">
+              <div class="card-body">
+                <h3 class="h3class">Log in</h3>
+                <form>
+                  <div class="form-group">
+                    <div style="margin-bottom:10px"><label for="exampleInputEmail1">Email address:</label></div>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="exampleInputEmail1"
+                      aria-describedby="emailHelp"
+                      placeholder="Enter email"
+                    />
+                  </div>
+                  <div class="form-group">
+                    <div style="margin-bottom:10px"><label for="exampleInputPassword1">Password: </label></div>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="exampleInputPassword1"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div class="form-group">
+                     <div style="margin-bottom:10px"><label for="exampleInputPassword1">Confirm Password:</label></div>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="exampleInputPassword1"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div class="submit-button">
+                    <b-button type="submit" variant="outline-dark"
+                      >Log in</b-button
+                    >
+                  </div>
+                  <RuleBox>
+                    <Hr />
+                    <div style="text-align:center">or</div>
+                    <Hr />
+                  </RuleBox>
+                  <div class="submit-button">
+                    <button variant="outline-dark">
+                      <img src="@/assets/google.png" /> Login with Google
+                    </button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div>
+      <Footer />
+    </div>
+  </div>
 </template>
-<style scoped>
-.submit-button{
-  margin-top: 10px;
-  text-align:center;
+<script>
+import Navbar from '@/components/Navbar'
+import SubNavBar from '@/components/SubNavbar'
+import Footer from '@/components/Footer'
+
+export default {
+  name: 'nav-footer',
+  components: {
+    Navbar,
+    SubNavBar,
+    Footer}
 }
-.h3class{
+</script>
+
+<style scoped>
+.submit-button {
+  margin-top: 10px;
   text-align: center;
 }
-.form-group{
+.h3class {
+  text-align: center;
+}
+.form-group {
   margin-bottom: 20px;
 }
 </style>
